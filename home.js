f95fd0e822540a5c1887de18ddae356d4d1403ae
gulam <gulam.3119027.co@mhssce.ac.in>
2021-10-20 20:55:51 +0530

    javasript

---
+++ home.js
@@ -0,0 +1,6 @@
+// console.log("hello gulam")
+// alert("warning, u are learnimg java script")
+// var a= "Azeem";
+// console.log(a)
+var JsAge= prompt("What is the your age")
+document.getElementById("fromIndex").innerHTML=JsAge;
